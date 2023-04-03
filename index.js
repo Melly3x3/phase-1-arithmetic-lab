f8a354400acd9f54658c8f48e2c2e5bc6304e8e8
Melvin <jmelvin1134@gmail.com>
2023-04-02 23:57:27 -0400

    part of arith lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+var num1 = 2
+var num2 = 31
+const multiply = num1 * num2;
+console.log(multiply(2, 31))
 
-// Write your code here
+var random
+const randomInt = Math.floor(Math.random() * 100) + 1; {
+    Number.isInteger(random);
+}
+console.log(randomInt);
+
+var num3 = 14
+var num4 = 10
+function mod(num3, num4) {
+    return num3 % num4
+}
+console.log(mod(14, 10))
+
+Math.max(10, 20)
